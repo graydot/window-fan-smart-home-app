0f8c743d4d8d9a5c044adc14c958b4fcc7658cc6
Jeba Singh Emmanuel <jebasingh.emmanuel@gmail.com>
2020-06-19 13:30:18 -0700

    Initial app with buttons ready. Next step, hook up api calls

--- App.js
+++ App.js
@@ -1,10 +1,45 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Button, StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
+  const [imageUrl, setImageUrl] = useState("https://reactnative.dev/docs/assets/p_cat2.png");
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Image 
+      source={{uri: imageUrl}}
+      style={styles.image}
+      />
+      <Button
+        onPress={()=>{
+
+        }}
+        title = "Power"
+        />
+      <Button
+        onPress={()=>{
+
+        }}
+        title = "Temp +"
+        />
+      <Button
+        onPress={()=>{
+
+        }}
+        title = "Temp -"
+        />
+      <Button
+        onPress={()=>{
+
+        }}
+        title = "Mode"
+        />
+      <Button
+        onPress={()=>{
+
+        }}
+        title = "Airflow"
+        />
     </View>
   );
 }
@@ -16,4 +51,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    width: 200,
+    height: 200,
+  }
 });
